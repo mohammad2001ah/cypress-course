475139c75b0ac07bc94575be5847a6ba2f6775cf
mohammad2001ah <mah.2001.ah@gmail.com>
2025-09-22 23:27:07 +0300

    second commitment is to implement the cypress functions

---
+++ cypress/e2e/mocha.cy.js
@@ -0,0 +1,48 @@
+/// <reference types="Cypress"/>
+
+/**
+ * describe is a function to put test case in suite
+ * tack 2 parameter 
+ * describe("name functionality", function {
+    * it ("test case name", function(){
+    * step to execute test case
+    * })
+ * })
+ * 
+ * 
+ * 
+ * it -> describe a test case in mocha
+ * it is a function in mocha to billed a test case 
+ * tack 2 parameter
+ * it ("test case name", function(){
+ * step to execute test case
+ * })
+ */
+
+//Login functionality
+describe("Login functionality",()=>{
+  it("Test Case #1: should login  with valid email and password correctly",function(){
+    cy.log("should login  with valid email and password correctly");
+  });
+  
+  it("Test Case #2: should not login if the user name is wrong", ()=>{
+    cy.log("I'm inside a test case")
+  });
+  
+  it("Test Case #3: Should be able to rest the password ", ()=>{
+    cy.log("I'm inside a test case ")
+  });
+  
+  it("Test Case #4: should not login if the user name is wrong", ()=>{
+    cy.log("I'm inside a test case ")
+  });
+  
+});
+
+//Order functionality
+describe("Order functionality",()=>{
+  it("Test Case #1: should be able to order a new food", ()=>{
+    cy.log("I'm inside a test case ")
+  });
+
+});

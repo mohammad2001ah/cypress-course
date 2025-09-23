97e024dcaa27f4b309da8d00fd349cbd0996ee86
mohammad2001ah <mah.2001.ah@gmail.com>
2025-09-23 15:19:39 +0300

    Update 02-visit.cy.js

--- cypress/e2e/02-visit.cy.js
+++ cypress/e2e/02-visit.cy.js
@@ -1,10 +1,9 @@
-
-
-
 // it("it should visit the website correctly",()=>{
 //   // Visit the website
 //     cy.visit('https://google.com');
 // });
+
+
 it("it should visit the homepage html file correctly",()=>{
   // Visit the website
     cy.visit('cypress/index.html');

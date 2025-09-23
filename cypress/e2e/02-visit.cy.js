bc305913bbc138f25c74a02265287436ca475a03
mohammad2001ah <mah.2001.ah@gmail.com>
2025-09-23 16:12:12 +0300

    ded

--- cypress/e2e/02-visit.cy.js
+++ cypress/e2e/02-visit.cy.js
@@ -1,10 +1,10 @@
-// it("it should visit the website correctly",()=>{
-//   // Visit the website
-//     cy.visit('https://google.com');
-// });
+it("it should visit the website correctly",()=>{
+  // Visit the website or localhost or html file
+    cy.visit('cypress/index.html');
+});
 
 
 it("it should visit the homepage html file correctly",()=>{
   // Visit the website
-    cy.visit('cypress/index.html');
+    cy.visit('https://google.com');
 });

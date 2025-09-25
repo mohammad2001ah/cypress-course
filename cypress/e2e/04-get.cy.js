dd0e959c7936e59adc93b30f857e9bc0c904b5b6
mohammad2001ah <mah.2001.ah@gmail.com>
2025-09-25 17:13:20 +0300

    command

--- cypress/e2e/04-get.cy.js
+++ cypress/e2e/04-get.cy.js
@@ -54,9 +54,64 @@ it("find the element from a list of elements by index",()=>{
 });
 
 //filter command
-it.only("find the element from a list of elements by filter",()=>{
+it("find the element from a list of elements by filter",()=>{
   cy.visit('cypress/index.html');
   //to find the element from a list of elements by filter we use filter()
   cy.get("li").filter(".web");
 });
+
+//find element by children command
+it("find the element from a list of elements by children",()=>{
+  cy.visit('cypress/index.html');
+  //to find the element from a list of elements by children we use children()
+  //we can use children() with selector
+  cy.get(".course-list").children();
+  //we can use children() without selector
+  cy.get(".course-list").children(".web");
+  cy.get(".course-list").children(".web").first();
+  cy.get(".course-list").children(".web").last();
+  cy.get(".course-list").children(".web").filter(".list7");
+  cy.get(".course-list").children(".web").eq(2);
+});
+
+//find element by find command
+it("find the element from a list of elements by find",()=>{
+  cy.visit('cypress/index.html');
+  //we can find  element based on the parent using the find command .find(css selector)
+  cy.get(".course-list").find(".web");
+});
+
+//find element by parent command
+it("find the element using the parent command",()=>{
+  cy.visit('cypress/index.html');
+  //we find the elemnet using the children we use parent() command
+  cy.get(".test1").parent();
+  
+});
+
+//find element by parents command
+it("find the element using the parents command",()=>{
+  cy.visit('cypress/index.html');
+  //we find the elemnet using the children we use parents() command
+  //parents will get all the parents of the element in an array
+  cy.get(".list1.web").parents();
+});
+
+//find element by siblings command
+it("find the element using the siblings command",()=>{
+  cy.visit('cypress/index.html');
+  //we find the elemnet using the children we use siblings() command
+  //siblings will get all the siblings(brother) of the element in an array
+  cy.get(".list1.web").siblings();
+  cy.get(".list1.web").siblings('.web');
+});
+
+//find element by text command
+it.only("find the element using the contains command",()=>{
+  cy.visit('cypress/index.html');
+  //we find the elemnet using the text we use contains() command
+  cy.contains("Appium");
+  cy.get('li').contains("Cypress");
+});
+
 

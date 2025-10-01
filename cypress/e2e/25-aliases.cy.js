685cde4314535a4bb0095b3332c6fe5289f26e31
mohammad2001ah <mah.2001.ah@gmail.com>
2025-10-01 14:53:28 +0300

    Update 25-aliases.cy.js

--- cypress/e2e/25-aliases.cy.js
+++ cypress/e2e/25-aliases.cy.js
@@ -4,3 +4,32 @@ it("Aliases in cypress", () => {
 
   cy.get("@url");
 });
+it("Aliases in cypress", () => {
+  cy.visit("cypress/index.html");
+  cy.url().as('url');
+  cy.get("@url").should("contain", "index.html");
+});
+
+//1- in aliases we have access just in the test cases
+//If we define aliases in a test case, we must use them in the same test case and not another one.
+it("Aliases in cypress", () => {
+  cy.visit("cypress/index.html");
+  cy.url().as('url');
+});
+it("Aliases in cypress", () => {
+  cy.get("@url").should("contain", "index.html");
+});
+// in this case the output will be undefined because the alias is defined in another test case
+
+describe.only("Aliases in cypress", () => {
+  beforeEach(() => {
+    cy.visit("cypress/index.html");
+    cy.url().as('url');
+  });
+  it("TC #1 :", () => {
+    cy.get("@url").should("contain", "index.html");
+  });
+  it("TC #2 :", () => {
+    cy.get("@url").should("contain", "index.html");
+  });
+});
